0402e81098e21f1a4c382eaff571e9c11571b7e3
ankitvashishta <ANKIT PC@ANKIT>
2017-05-03 22:04:26 +0530

    SynchronizeAndWaitOnDifferentObjects

---
+++ SCJP PreparationsOffice/src/chapter9/ThreadInteraction/SynchronizeAndWaitOnDifferentObjects.java
@@ -0,0 +1,36 @@
+package chapter9.ThreadInteraction;
+
+public class SynchronizeAndWaitOnDifferentObjects {
+
+	public static void main(String args[]) {
+		Thread thread1 = new Thread(new ClassToNotify());
+		thread1.start();
+		synchronized (thread1) {
+			try {
+				thread1.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
+
+class ClassToNotify implements Runnable {
+	StringBuffer str = new StringBuffer();
+
+	@Override
+	public void run() {
+		int i;
+		synchronized (str) {
+			System.out.println("This block synchronizes on StringBuffer object and calls"
+					+ " notify on 'this' ie. currently executing object. ");
+			System.out.println("A runtime exception would be thrown - 'IllegalMonitorStateException'");
+			for (i = 0; i < 10; i++) {
+				i++;
+			}
+			System.out.println("The Sum is : " + i);
+			notify();
+		}
+	}
+}
